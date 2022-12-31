abf08fb17aa44a5f3b5e3c7c03da2a8f347d215e
Sunil Sakharam Bombe <46067272+sunilbombe456@users.noreply.github.com>
2022-12-31 09:39:38 -0800

    commit

--- TestNgAutomationFramework/src/test/java/com/webwork/app/runner/Runner.java
+++ TestNgAutomationFramework/src/test/java/com/webwork/app/runner/Runner.java
@@ -1,10 +1,38 @@
 package com.webwork.app.runner;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//System.setProperty("","")
+		System.setProperty("webdriver.chrome.driver", "C://drivers//chromedriver.exe");  
+		
+		//WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://rahulshettyacademy.com/client");
+		
+		driver.findElement(By.cssSelector("input[type='email']")).sendKeys("ganesh.jadhav@gmail.com");
+		
+		driver.findElement(By.cssSelector("input[type='password']")).sendKeys("Ganesh@123");
+		
+		driver.findElement(By.cssSelector("input[type='submit']")).click();
+				
+		//driver.close();
 	}
 
 }
